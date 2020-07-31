06ebbe537790517d5a61082c94bedb701ff06e6b
JordanC <jcwilliams410@gmail.com>
2020-07-31 15:53:42 -0400

    server update 1

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+//DEPENDENCIES
+require("dotenv").config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const morgan = require("morgan");
+const cors = require("cors");
+
+//GLOBAL VARIABLES
 const PORT = process.env.PORT;
+const NODE_ENV = process.env.NODE_ENV;
+const mongoURI = process.env.mongoURI;
+//const db = mongoose.connection
+//const mongoConfigObject = {useNewURLParser: true, useUnifiedTopology: true};
 
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
 app.listen(PORT, () => {
-    
+    console.log(`Listening on port ${PORT}`);
 });
