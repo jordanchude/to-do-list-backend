e4c498a452e79bb7cd71da37e4627ed2ab130496
JordanC <jcwilliams410@gmail.com>
2020-07-31 10:50:10 -0400

    app gett fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
